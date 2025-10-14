8d2b7c59a7199ca721b24d16030d81612f45f361
danitec <l21590051@sjuanrio.tecnm.mx>
2025-10-14 17:49:32 -0600

    prueba 2

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app =  Flask(__name__)
 #Ruta raiz
 @app.route('/')
 def index():
-    return 'Hola Mundo esto es una prueba'
+    return 'Hola, Mundo! Esta es una prueba 2'
 
 if __name__ == '__main__':
     app.run(debug=True)
